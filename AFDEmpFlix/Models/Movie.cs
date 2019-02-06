2323644fb0646b0945302d296f92e881b4d1d1d6
fedonman <fedonman@gmail.com>
2019-02-06 18:56:02 +0200

    adds Movies model

---
+++ AFDEmpFlix/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AFDEmpFlix.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Duration { get; set; }
+        public string TrailerUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+
+    public enum Genre
+    {
+        Action,
+        Horror,
+        ScienceFiction,
+        Comedy
+    }
+}
