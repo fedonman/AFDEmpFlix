8f6334541ae91fe3769fe5918a2ea4cc5052edc7
fedonman <fedonman@gmail.com>
2019-02-06 19:09:05 +0200

    adds migrations and updates database

---
+++ AFDEmpFlix/Migrations/201902061703455_First Migration.cs
@@ -0,0 +1,30 @@
+namespace AFDEmpFlix.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.Int(nullable: false),
+                        Duration = c.Int(nullable: false),
+                        TrailerUrl = c.String(),
+                        ThumbnailUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
--- AFDEmpFlix/Models/AppContext.cs
+++ AFDEmpFlix/Models/AppContext.cs
@@ -6,19 +6,10 @@ namespace AFDEmpFlix.Models
 
     public class AppContext : DbContext
     {
-        // Your context has been configured to use a 'AppContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'AFDEmpFlix.Models.AppContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'AppContext' 
-        // connection string in the application configuration file.
         public AppContext()
             : base("name=AppContext")
         {
         }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         public virtual DbSet<Movie> Movies { get; set; }
     }
